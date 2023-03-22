8bbbe5dadda0777f387880278e60f946984775a8
AlexandrLokhov <89500883114@maile.ru>
2023-03-22 13:55:13 +0600

    day_10

---
+++ 10 - Hold Shift and Check Checkboxes/app.js
@@ -0,0 +1,16 @@
+const checkboxes = document.querySelectorAll(`[type='checkbox']`);
+
+let lastChecked;
+
+function handleCheck(event) {
+    let inBetween = false;
+    if (event.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            if (checkbox === this || checkbox === lastChecked) inBetween = !inBetween;
+            if (inBetween) checkbox.checked = true;
+        });
+    }
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
