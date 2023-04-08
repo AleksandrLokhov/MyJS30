8dc0121f6ed8064dfdd28f506a079038289d7d9b
AlexandrLokhov <89500883114@maile.ru>
2023-04-08 16:18:59 +0600

    days_29_30

--- 23 - Speech Synthesis/app.js
+++ 23 - Speech Synthesis/app.js
@@ -10,7 +10,7 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
     voices = this.getVoices();
     voicesDropdown.innerHTML = voices
-        .filter(voice => voice.lang.includes('en'))
+        .filter(voice => voice.lang.includes('ru'))
         .map(voice => `<option value="${voice.name}">${voice.name}(${voice.lang})</option>`)
         .join('');
 }
